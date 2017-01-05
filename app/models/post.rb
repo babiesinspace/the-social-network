04c43ff1ab0f1468754b640fd90e228f426714a2
Patrick Kolodgy <email": null>
2017-01-05 16:58:42 -0500

    Assoc and validations for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-  
+  belongs_to :author, class_name: User
+  belongs_to :receiver, class_name: User
+
+  validates :body, presence: true, length: {minimum: 10 }
 end
