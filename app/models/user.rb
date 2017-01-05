63b74ac44202a2ead7957614750b2d2edc8e5be2
Patrick Kolodgy <email": null>
2017-01-05 16:54:00 -0500

    Friendship schema and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
-  has_and_belongs_to_many :friends,
-                          :class_name => "User",
-                          :join_table => "friendships",
-                          :foreign_key => "user_1_id",
-                          :association_foreign_key => "user_2_id"
+  # has_and_belongs_to_many :friends,
+  #                         :class_name => "User",
+  #                         :join_table => "friendships",
+  #                         :foreign_key => "user_1_id",
+  #                         :association_foreign_key => "user_2_id"
 
+  has_many :friendships, foreign_key: :user_1_id
+  has_many :friends, through: :friendships, source: :user_2
 
+  has_many :inverse_friendships, class_name: Friendship, foreign_key: :user_2_id
+
+  has_many :inverse_friends, through: :inverse_friendships, source: :user_1
+
+  has_many :friend_requests, foreign_key: :requester_id
+  has_many :pending_requests, class_name: FriendRequest, foreign_key: :receiver_id
 
   def add_friend(friend)
     self.friends << friend unless self.friends.include?(friend) || friend == self
+  end
+
+  def all_friends
+    (self.friends + self.inverse_friends).uniq
+  end
+
+  def request_friend(friend)
+    @request = FriendRequest.new(requester: self, receiver: friend)
+    if @request.save
+      "Request sent"
+    else
+      "Problem with request"
+    end
   end
 end
