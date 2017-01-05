2daf98b5509a3d30e8e080af1d76565c767fddea
Patrick Kolodgy <email": null>
2017-01-05 15:20:31 -0500

    Create Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105200007) do
+ActiveRecord::Schema.define(version: 20170105200915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.integer  "requester_id",                 null: false
+    t.integer  "receiver_id",                  null: false
+    t.boolean  "accepted",     default: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_1_id",  null: false
+    t.integer  "user_2_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body",        null: false
+    t.integer  "author_id",   null: false
+    t.integer  "receiver_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +51,12 @@ ActiveRecord::Schema.define(version: 20170105200007) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "full_name"
+    t.date     "birthday"
+    t.string   "hometown"
+    t.string   "education"
+    t.string   "relationship_status"
+    t.string   "current_city"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
