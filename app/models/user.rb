72d409bb61acf2c6be4d9b7ba28d8d5e6be53e18
Patrick Kolodgy <email": null>
2017-01-05 16:02:45 -0500

    Update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_and_belongs_to_many :friends,
+                          :class_name => "User",
+                          :join_table => "friendships",
+                          :foreign_key => "user_1_id",
+                          :association_foreign_key => "user_2_id"
+
+
+
+  def add_friend(friend)
+    self.friends << friend unless self.friends.include?(friend) || friend == self
+  end
 end
