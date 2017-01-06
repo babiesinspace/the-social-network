bd3b71016a4134bc746231c776b24b9d299589bb
Patrick Kolodgy <email": null>
2017-01-06 08:55:03 -0500

    Clean up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: 'profiles#index'
-  get '/profiles', to: 'profiles#index'
-  get 'profiles/:id/edit', to: 'profiles#edit', as: :edit_profile
-  get '/profiles/:id', to: 'profiles#show'
+  # get '/profiles', to: 'profiles#index'
+  # get 'profiles/:id/edit', to: 'profiles#edit', as: :edit_profile
+  # get '/profiles/:id', to: 'profiles#show'
   get '/logout', to: 'profiles#destroy'
+
+  resources :profiles, only: [:new, :create, :index, :edit] do
+    resources :posts, only: [:create]
+  end
+
   devise_for :users, :controllers => {:registrations => "registrations"}
 end
