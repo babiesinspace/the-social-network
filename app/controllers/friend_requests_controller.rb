0f4558da45693b1f1f6042f771af37874eaebe1f
Patrick Kolodgy <email": null>
2017-01-06 11:40:09 -0500

    add create controller for friend requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,23 +1,36 @@
 class FriendRequestsController < ApplicationController
   # before_action: :authenticate_user!
 
-def index
-  user = User.find(params[:profile_id])
-  pending_requests = {}
-  pending_requests[:sent_requests] = user.sent_requests
-  pending_requests[:received_requests] = user.received_requests
-  respond_to do |format|
-    format.html
-    format.json { render json: pending_requests }
+  def index
+    user = User.find(params[:profile_id])
+    pending_requests = {}
+    pending_requests[:sent_requests] = user.sent_requests
+    pending_requests[:received_requests] = user.received_requests
+    respond_to do |format|
+      format.html
+      format.json { render json: pending_requests }
+    end
   end
-end
 
-def create
+  def create
+    sender = current_user
+    receiver = User.find(params[:profile_id])
+    request = FriendRequest.new(friend_requests_params)
+    if request.save
+      flash[:notice] = "Request Sent!"
+    else
+      flash[:error] = "There was a problem creating your post!"
+    end
+  end
 
-end
+  def destroy
 
-def destroy
+  end
 
-end
+  private
+
+  def friend_requests_params
+    params.require(:friend_request).permits(:requester_id, :receiver_id)
+  end
 
 end
