a814618c8d6bbfbeae921cbb2732517cc28b6023
Patrick Kolodgy <email": null>
2017-01-06 17:58:18 -0500

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-50.times do
+100.times do
   internet_name = Faker::Internet.user_name
   User.create(email: Faker::Internet.email, password: 'password', full_name: Faker::Name.name, birthday: Faker::Date.birthday,
   hometown: Faker::Address.city, current_city: Faker::Address.city, education: Faker::Educator.university, pic_url: Faker::Avatar.image(internet_name))
 end
 
-500.times do
+1000.times do
   Post.create(body: Faker::Lorem.paragraph(2), author: User.all.sample, receiver: User.all.sample)
 end
 
-500.times do
+1000.times do
   User.all.sample.add_friend(User.all.sample)
 end
