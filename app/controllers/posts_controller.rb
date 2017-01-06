3315018e87ebe50de8efec0b3053cb4087b3c46f
Patrick Kolodgy <email": null>
2017-01-06 10:55:55 -0500

    add create post method, untested and must remember to try

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,29 @@ class PostsController < ApplicationController
     end
   end
 
+#Untested (must first create form)
   def create
-
+    author = current_user
+    receiver = User.find(params[:profile_id])
+    post = Post.new(post_params)
+    if post.save
+      respond_to do |format|
+        format.html { redirect_to profile_url(receiver) }
+        format.json { render json: post }
+      end
+    else
+      flash[:error] = "There was a problem creating your post!"
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permits(:body, :author_id, :receiver_id)
   end
 
 end
