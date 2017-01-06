5ba699d0d04a9e635f02dbcec3b9e14a859d7750
Patrick Kolodgy <email": null>
2017-01-06 13:44:46 -0500

    Fix Profile edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "profiles#index"
-  get '/profiles/:id/edit', to: 'profiles#index'
   # get '/profiles', to: 'profiles#index'
   # get 'profiles/:id/edit', to: 'profiles#edit', as: :edit_profile
   # get '/profiles/:id', to: 'profiles#show'
